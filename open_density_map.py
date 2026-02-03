b9912d198d219bfa34592e41aeff564407585483
Jason Porta <jason.porta@protonmail.com>
2026-02-03 13:51:05 -0500

    Delete open_density_map.py

--- open_density_map.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-# Open density map in binary mode for read only. Used with the 'mrcfile' I/O library 
-with open('apoferritin.mrc', 'rb') as f:
-   byte=f.read(1)
-   while byte != "":
-      byte=f.read(1) 
